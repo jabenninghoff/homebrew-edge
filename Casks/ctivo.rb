abdef3022b2c604f0c5b725315de49da8b2ea6e4
jabenninghoff <jbenninghoff@mac.com>
2019-10-29 22:31:47 -0500

    Update ctivo to 3.4.0

--- Casks/ctivo.rb
+++ Casks/ctivo.rb
@@ -1,8 +1,8 @@
 cask 'ctivo' do
-  version '3.3.1'
+  version '3.4.0'
   sha256 'c7318a48d1fa3aaf10999051c37b018e9cf3c9ae413182a7d235e66f9ef08ab1'
 
-  url "https://github.com/mackworth/cTiVo/releases/download/#{version}/cTiVo.#{version}.final.zip"
+  url "https://github.com/mackworth/cTiVo/releases/download/#{version}/cTiVo.zip"
   appcast 'https://github.com/mackworth/cTiVo/releases.atom'
   name 'cTiVo'
   homepage 'https://github.com/mackworth/cTiVo'
