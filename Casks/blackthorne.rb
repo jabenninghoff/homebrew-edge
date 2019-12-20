e86250075b6917911b828e6d97f0209cdc6ebea5
jabenninghoff <jbenninghoff@mac.com>
2019-12-19 22:09:16 -0600

    Add Blackthorne Cask

---
+++ Casks/blackthorne.rb
@@ -0,0 +1,15 @@
+cask 'blackthorne' do
+  version '1.0.0.0'
+  sha256 '02bae02aa32e6bcb38e607a506f343bd743816796b16f08836e7757f869767ce'
+
+  url 'http://dist.blizzard.com/downloads/blackthorne/5e1a0ce1d84e3ee3005dd26181e14a2f/BlackthorneSetup.dmg'
+  name 'Blackthorne'
+  homepage 'https://www.blizzard.com/en-us/download/?platform=macos'
+
+  app 'Blackthorne.app'
+
+  zap trash: [
+               '~/Library/Application Support/Blizzard/Blackthorne',
+               '~/Library/Saved Application State/com.blizzard.blackthorne.savedState'
+             ]
+end
