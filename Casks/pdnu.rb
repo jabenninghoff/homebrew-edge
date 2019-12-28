517ebf03bbe36fbb8aa2c71a0933ca2ebd4f5ebe
jabenninghoff <jbenninghoff@mac.com>
2019-12-28 15:26:05 -0600

    Add CyberPower PDNU

---
+++ Casks/pdnu.rb
@@ -0,0 +1,15 @@
+cask 'pdnu' do
+  version '1.1'
+  sha256 '24097099e3798669854d36dd68031b8ddb6f0498dfe7e96544379ac2f77ff037'
+
+  url 'https://www.cyberpower.com/global/en/File/GetImageByDocId/SU-18070002-10'
+  name 'Power Device Network Utility'
+  homepage 'https://www.cyberpower.com/eu/en/software/utility'
+
+  app 'PDNU.app'
+
+  zap trash: [
+               '~/Library/Preferences/com.cyberpower.PDNU.plist',
+               '~/Library/Library/Saved Application State/com.cyberpower.PDNU.savedState',
+             ]
+end
