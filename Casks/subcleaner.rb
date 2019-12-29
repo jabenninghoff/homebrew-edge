b4bfc1a34b0130bdcf28bf4d10d6ad8cfa85666c
jabenninghoff <jbenninghoff@mac.com>
2019-12-28 18:18:46 -0600

    Add SubCleaner

---
+++ Casks/subcleaner.rb
@@ -0,0 +1,10 @@
+cask 'subcleaner' do
+  version '1.0b7'
+  sha256 '00ea7e1f1afb22a3d21c34cbff61cc222f1f6465c952d4a6c853f8d92820cb0f'
+
+  url "https://www.rivola.net/files/subcleaner_#{version}.dmg"
+  name 'SubCleaner'
+  homepage 'https://www.rivola.net/subcleaner/'
+
+  app 'SubCleaner.app'
+end
