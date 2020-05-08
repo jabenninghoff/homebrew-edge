82637bef629b4f622280d8cfbcbe7f4c681c3b8c
jabenninghoff <jbenninghoff@mac.com>
2020-05-08 07:06:38 -0500

    Add Magic Window Screen Saver 2.1.1

---
+++ Casks/magic-window-screen-saver.rb
@@ -0,0 +1,13 @@
+cask 'magic-window-screen-saver' do
+  version '2.1.1'
+  sha256 '6161ed89ff2cd310a9ca4ac70350e55d30363b77a3a9db5fd7ac445e4e0e899d'
+
+  # magicwindowmac.jetsoncreativell.netdna-cdn.com was verified as official when first introduced to the cask
+  url 'http://magicwindowmac.jetsoncreativell.netdna-cdn.com/apps/MagicWindowScreenSaverPlugin.dmg'
+  name 'Magic Window Screen Saver'
+  homepage 'https://www.jetsoncreative.com/screensaverplugin/'
+
+  screen_saver 'Magic Window.saver'
+
+  caveats 'For macOS Catalina you must provide Apple’s legacyScreenSaver process with full disk access to allow the screen saver to access your downloaded scenes.'
+end
