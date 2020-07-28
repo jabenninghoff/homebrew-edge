fdcafedf05881ac819ae782625de9c0ecbbddd1b
jabenninghoff <jbenninghoff@mac.com>
2020-07-27 23:45:35 -0500

    Add license per brew audit --new-formula

--- Formula/apg.rb
+++ Formula/apg.rb
@@ -3,6 +3,7 @@ class Apg < Formula
   homepage "https://github.com/jabenninghoff/apg"
   url "https://github.com/jabenninghoff/apg/archive/v2.2.3.tar.gz"
   sha256 "33a3efa2d02ffb95e00175c1e23f9f32f20a97889087e67f6086d4fa4c3854a1"
+  license "BSD-3-Clause"
 
   def install
     system "make", "standalone",
