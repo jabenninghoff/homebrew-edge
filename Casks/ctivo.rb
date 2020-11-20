b94ac625399ffbb2ba79113127b2b5519705ef93
John Benninghoff <jbenninghoff@mac.com>
2020-11-20 09:40:06 -0600

    Update ctivo from 3.4.2 to 3.4.3

--- Casks/ctivo.rb
+++ Casks/ctivo.rb
@@ -1,6 +1,6 @@
 cask "ctivo" do
-  version "3.4.2"
-  sha256 "0a4bb4ecfbb7da407193d2db2d218694864ecddb0f2c9a421856a51a100933ca"
+  version "3.4.3"
+  sha256 "78dde65d6c54afa7a7bf9071e4926e752ce02b3ae82d5688b19545bed90c44a5"
 
   url "https://github.com/mackworth/cTiVo/releases/download/#{version}/cTiVo.app.zip"
   appcast "https://github.com/mackworth/cTiVo/releases.atom"
