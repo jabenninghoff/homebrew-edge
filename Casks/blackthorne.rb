cd2d4b9de1d03abe6aa3c76623926d085993f408
John Benninghoff <jbenninghoff@mac.com>
2020-11-21 10:22:17 -0600

    Add desc to blackthorne

--- Casks/blackthorne.rb
+++ Casks/blackthorne.rb
@@ -4,6 +4,7 @@ cask "blackthorne" do
 
   url "http://dist.blizzard.com/downloads/blackthorne/5e1a0ce1d84e3ee3005dd26181e14a2f/BlackthorneSetup.dmg"
   name "Blackthorne"
+  desc "Cinematic platform game"
   homepage "https://www.blizzard.com/en-us/download/?platform=macos"
 
   app "Blackthorne.app"
