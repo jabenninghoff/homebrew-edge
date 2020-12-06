cea4d4853df4abc7ad7840a3c6887c05632c1571
John Benninghoff <jbenninghoff@mac.com>
2020-12-06 10:55:23 -0600

    Update netnewswire5.rb

--- Casks/netnewswire5.rb
+++ Casks/netnewswire5.rb
@@ -2,8 +2,8 @@ cask "netnewswire5" do
   version "5.0.4"
   sha256 "6223a5fecae3307016bb645c49e4988fb5a253460f9a257e47fa5bbe0d89ca3d"
 
-  # github.com/brentsimmons/NetNewsWire/ was verified as official when first introduced to the cask
-  url "https://github.com/brentsimmons/NetNewsWire/releases/download/mac-#{version}/NetNewsWire#{version}.zip"
+  url "https://github.com/brentsimmons/NetNewsWire/releases/download/mac-#{version}/NetNewsWire#{version}.zip",
+      verified: "github.com/brentsimmons/NetNewsWire"
   appcast "https://github.com/brentsimmons/NetNewsWire/releases.atom"
   name "NetNewsWire"
   desc "Free and open-source RSS reader"
