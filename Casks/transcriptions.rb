9b53147758126e77ee5666144f2c1dd3a9163df2
John Benninghoff <jbenninghoff@mac.com>
2020-12-06 11:00:15 -0600

    Update transcriptions.rb

--- Casks/transcriptions.rb
+++ Casks/transcriptions.rb
@@ -2,8 +2,8 @@ cask "transcriptions" do
   version "1.3"
   sha256 "c0aa6654c16dc05195cc88113323f2cfe2ce2a0a5ea85e95e42692d0b144205e"
 
-  # github.com/soleil-alpin was verified as official when first introduced to the cask
-  url "https://github.com/soleil-alpin/Transcriptions/releases/download/v#{version}/Transcriptions.app.zip"
+  url "https://github.com/soleil-alpin/Transcriptions/releases/download/v#{version}/Transcriptions.app.zip",
+      verified: "github.com/soleil-alpin/Transcriptions"
   appcast "https://github.com/soleil-alpin/Transcriptions/releases.atom"
   name "Transcriptions"
   desc "Text editor for fast manual transcription"
