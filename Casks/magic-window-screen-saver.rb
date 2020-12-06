61ccc53711e54c7cb07030d271e34860d8958a1f
John Benninghoff <jbenninghoff@mac.com>
2020-12-06 10:51:40 -0600

    Update magic-window-screen-saver.rb

--- Casks/magic-window-screen-saver.rb
+++ Casks/magic-window-screen-saver.rb
@@ -2,8 +2,8 @@ cask "magic-window-screen-saver" do
   version "2.1.1"
   sha256 "6161ed89ff2cd310a9ca4ac70350e55d30363b77a3a9db5fd7ac445e4e0e899d"
 
-  # magicwindowmac.jetsoncreativell.netdna-cdn.com was verified as official when first introduced to the cask
-  url "http://magicwindowmac.jetsoncreativell.netdna-cdn.com/apps/MagicWindowScreenSaverPlugin.dmg"
+  url "http://magicwindowmac.jetsoncreativell.netdna-cdn.com/apps/MagicWindowScreenSaverPlugin.dmg",
+      verified: "magicwindowmac.jetsoncreativell.netdna-cdn.com"
   name "Magic Window Screen Saver"
   desc "Screen saver plug-in"
   homepage "https://www.jetsoncreative.com/screensaverplugin/"
