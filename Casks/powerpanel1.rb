80da758d579148e4874a74040828c398850cb5f9
John Benninghoff <jbenninghoff@mac.com>
2020-12-06 10:56:34 -0600

    Update powerpanel1.rb

--- Casks/powerpanel1.rb
+++ Casks/powerpanel1.rb
@@ -2,8 +2,8 @@ cask "powerpanel1" do
   version "1.1.0"
   sha256 "9ef3160ad516cf246647a11bdbeaa756a98b2c0d9915ccabbf0d6cc72bebdf51"
 
-  # dl4jz3rbrsfum.cloudfront.net/ was verified as official when first introduced to the cask
-  url "https://dl4jz3rbrsfum.cloudfront.net/software/ppm#{version.no_dots}.dmg"
+  url "https://dl4jz3rbrsfum.cloudfront.net/software/ppm#{version.no_dots}.dmg",
+      verified: "dl4jz3rbrsfum.cloudfront.net"
   name "CyberPower PowerPanel Personal"
   desc "Monitor and configure UPS"
   homepage "https://www.cyberpowersystems.com/products/software/power-panel-personal/"
