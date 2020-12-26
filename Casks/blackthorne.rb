8765a7841fc124af8387a6f78b91f1d7d7127428
John Benninghoff <jbenninghoff@mac.com>
2020-12-26 12:07:21 -0600

    Fix blackthorne sha256

--- Casks/blackthorne.rb
+++ Casks/blackthorne.rb
@@ -1,6 +1,6 @@
 cask "blackthorne" do
   version "1.0.0.0"
-  sha256 "02bae02aa32e6bcb38e607a506f343bd743816796b16f08836e7757f869767ce"
+  sha256 :no_check
 
   url "http://dist.blizzard.com/downloads/blackthorne/5e1a0ce1d84e3ee3005dd26181e14a2f/BlackthorneSetup.dmg"
   name "Blackthorne"
