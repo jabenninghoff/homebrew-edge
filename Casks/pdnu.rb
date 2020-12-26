13a2cdaa96ec483488991ae2bb5e52a4098bb186
John Benninghoff <jbenninghoff@mac.com>
2020-12-26 12:08:56 -0600

    Fix pdnu url, version

--- Casks/pdnu.rb
+++ Casks/pdnu.rb
@@ -1,8 +1,8 @@
 cask "pdnu" do
-  version "1.1"
+  version "1.1,18070002-10"
   sha256 "24097099e3798669854d36dd68031b8ddb6f0498dfe7e96544379ac2f77ff037"
 
-  url "https://www.cyberpower.com/global/en/File/GetImageByDocId/SU-18070002-10"
+  url "https://www.cyberpower.com/global/en/File/GetImageByDocId/SU-#{version.after_comma}"
   name "Power Device Network Utility"
   desc "Network device discovery utility"
   homepage "https://www.cyberpower.com/eu/en/software/utility"
