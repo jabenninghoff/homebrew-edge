0771e6a230e180e1576bf6b8d829341fa4294473
John Benninghoff <jbenninghoff@mac.com>
2021-02-06 12:20:32 -0600

    Modify caldigit-thunderbolt-charging to install once

--- Casks/caldigit-thunderbolt-charging-installer.rb
+++ Casks/caldigit-thunderbolt-charging-installer.rb
@@ -1,9 +1,12 @@
-cask "caldigit-thunderbolt-charging" do
-  version :latest
+cask "caldigit-thunderbolt-charging-installer" do
+  # clone of https://github.com/Homebrew/homebrew-cask-drivers/blob/HEAD/Casks/caldigit-thunderbolt-charging.rb
+  # modified to install once
+  version "installer"
   sha256 :no_check
 
   url "https://downloads.caldigit.com/CalDigit-Thunderbolt-Station-Mac-Drivers.zip"
   name "CalDigit Thunderbolt Station USB Charging & SuperDrive Support Driver"
+  desc "Apple SuperDrive, Apple Keyboard, and Improved iPhone/iPad Charging"
   homepage "https://www.caldigit.com/"
 
   pkg "CalDigit-Thunderbolt-Station-Mac-Drivers.pkg"
