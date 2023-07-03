1e440293cfe6ec663087a8bc6110e965a555159c
John Benninghoff <jbenninghoff@mac.com>
2023-07-03 11:27:37 -0500

    Add microsoft-edge114 from homebrew-cask

---
+++ Casks/microsoft-edge114.rb
@@ -0,0 +1,56 @@
+cask "microsoft-edge114" do
+  sha256 arm:   "3c033cf8bf635c26529eef47c61bfdf432ab2ccf026c6ba354bbff8971f82591",
+         intel: "c45d175530f5afb2172758be06140cf7a0cf807a00a2d8f2b6f07ce08fa4f5ed"
+
+  on_arm do
+    version "114.0.1823.58,6a5ef55e-f89f-44a1-8c29-76f02b3d6b0a"
+  end
+  on_intel do
+    version "114.0.1823.58,58781846-b82a-4306-aed9-1f9bd765a3d9"
+  end
+
+  url "https://msedge.sf.dl.delivery.mp.microsoft.com/filestreamingservice/files/#{version.csv.second}/MicrosoftEdge-#{version.csv.first}.pkg"
+  name "Microsoft Edge"
+  desc "Web browser"
+  homepage "https://www.microsoft.com/edge"
+
+  livecheck do
+    skip "legacy version for Mojave"
+  end
+
+  auto_updates true
+
+  pkg "MicrosoftEdge-#{version.csv.first}.pkg",
+      choices: [
+        {
+          "choiceIdentifier" => "com.microsoft.package.Microsoft_AutoUpdate.app", # Office16_all_autoupdate.pkg
+          "choiceAttribute"  => "selected",
+          "attributeSetting" => 0,
+        },
+      ]
+
+  uninstall pkgutil:   "com.microsoft.edgemac",
+            launchctl: [
+              "com.microsoft.EdgeUpdater.update-internal.109.0.1518.89.system",
+              "com.microsoft.EdgeUpdater.update.system",
+              "com.microsoft.EdgeUpdater.wake.109.0.1518.89.system",
+            ]
+
+  zap delete: "/Library/Application Support/Microsoft/EdgeUpdater",
+      rmdir:  "/Library/Application Support/Microsoft",
+      trash:  [
+        "~/Library/Application Scripts/com.microsoft.edgemac.wdgExtension",
+        "~/Library/Application Support/Microsoft Edge",
+        "~/Library/Application Support/Microsoft/EdgeUpdater",
+        "~/Library/Caches/com.microsoft.edgemac",
+        "~/Library/Caches/com.microsoft.EdgeUpdater",
+        "~/Library/Caches/Microsoft Edge",
+        "~/Library/Containers/com.microsoft.edgemac.wdgExtension",
+        "~/Library/HTTPStorages/com.microsoft.edge*",
+        "~/Library/LaunchAgents/com.microsoft.EdgeUpdater.*.plist",
+        "~/Library/Microsoft/EdgeUpdater",
+        "~/Library/Preferences/com.microsoft.edgemac.plist",
+        "~/Library/Saved Application State/com.microsoft.edgemac.*",
+        "~/Library/WebKit/com.microsoft.edgemac",
+      ]
+end
