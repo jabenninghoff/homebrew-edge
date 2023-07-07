6adc16f546afe403a1b6b8ed20f5dac4300981fe
John Benninghoff <jbenninghoff@mac.com>
2023-07-07 09:59:35 -0500

    Replace colossus appcast with livecheck

--- Casks/colossus.rb
+++ Casks/colossus.rb
@@ -3,10 +3,14 @@ cask "colossus" do
   sha256 "460aa15f6bbdc24935b8182a1660fcbe86a6ceeaa48bd52b87cd8578385e3061"
 
   url "https://downloads.sourceforge.net/colossus/Colossus-Public-Build-#{version}.zip"
-  appcast "https://sourceforge.net/projects/colossus/rss"
   name "Colossus"
   desc "Titan java clone"
-  homepage "http://colossus.sourceforge.net/"
+  homepage "https://colossus.sourceforge.net/"
+
+  livecheck do
+    url "https://sourceforge.net/projects/colossus/rss"
+    regex(%r{url=.*/Colossus-Public-Build-(\d+)\.zip}i)
+  end
 
   # stage_only and link to the staged path to work around the lack of a subfolder that can be targeted by suite
   stage_only true
