6e1b7d2b555889d009bc4048def06fa4cd74b7ce
John Benninghoff <jbenninghoff@mac.com>
2023-07-07 10:15:12 -0500

    Replace netnewswire5 appcast with livecheck

--- Casks/netnewswire5.rb
+++ Casks/netnewswire5.rb
@@ -3,11 +3,14 @@ cask "netnewswire5" do
   sha256 "6223a5fecae3307016bb645c49e4988fb5a253460f9a257e47fa5bbe0d89ca3d"
 
   url "https://github.com/brentsimmons/NetNewsWire/releases/download/mac-#{version}/NetNewsWire#{version}.zip",
-      verified: "github.com/brentsimmons/NetNewsWire"
-  appcast "https://github.com/brentsimmons/NetNewsWire/releases.atom"
+      verified: "github.com/brentsimmons/NetNewsWire/"
   name "NetNewsWire"
   desc "Free and open-source RSS reader"
   homepage "https://ranchero.com/netnewswire/"
+
+  livecheck do
+    skip "legacy version for Mojave"
+  end
 
   auto_updates true
 
