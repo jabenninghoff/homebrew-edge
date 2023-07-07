6d00a3569ac0d147249fc52e56a42cb68c126f2e
John Benninghoff <jbenninghoff@mac.com>
2023-07-07 10:09:18 -0500

    brew style --fix canoscan-toolbox

--- Casks/canoscan-toolbox.rb
+++ Casks/canoscan-toolbox.rb
@@ -3,7 +3,7 @@ cask "canoscan-toolbox" do
   sha256 "fa5442c719771b87dcefd59426efcf1743e8e024287a310eb1518cbd4c4907aa"
 
   url "https://gdlp01.c-wss.com/gds/1/0900002921/03/cstbosx#{version.no_dots}ej4.dmg",
-      verified: "gdlp01.c-wss.com/gds"
+      verified: "gdlp01.c-wss.com/gds/"
   name "CanoScan Toolbox"
   desc "Scanner software"
   homepage "https://www.usa.canon.com/internet/portal/us/home/support/details/scanners/support-scanners-canoscan-series/canoscan-lide-500f"
