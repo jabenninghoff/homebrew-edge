8047c67a666515c804d9013b60790a24837acbda
John Benninghoff <jbenninghoff@mac.com>
2023-07-07 11:11:27 -0500

    Remove nicecast appcast

--- Casks/nicecast.rb
+++ Casks/nicecast.rb
@@ -3,7 +3,6 @@ cask "nicecast" do
   sha256 "99ea875e2e4c8bfd7a976150140f25c85e006fdad2dc7124f7ae61fcba940537"
 
   url "https://rogueamoeba.com/legacy/downloads/Nicecast-#{version.no_dots}.zip"
-  appcast "https://rogueamoeba.net/ping/versionCheck.cgi?format=sparkle&bundleid=com.rogueamoeba.Nicecast&system=10114"
   name "Nicecast"
   desc "Internet radio broadcasting app"
   homepage "https://rogueamoeba.com/nicecast/"
