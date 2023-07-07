bb26ef4a14d3a9395ee9f827cc334e492d77c864
John Benninghoff <jbenninghoff@mac.com>
2023-07-07 10:48:58 -0500

    brew style --fix pdnu

--- Casks/pdnu.rb
+++ Casks/pdnu.rb
@@ -2,7 +2,7 @@ cask "pdnu" do
   version "1.1,18070002-10"
   sha256 "24097099e3798669854d36dd68031b8ddb6f0498dfe7e96544379ac2f77ff037"
 
-  url "https://www.cyberpower.com/global/en/File/GetImageByDocId/SU-#{version.after_comma}"
+  url "https://www.cyberpower.com/global/en/File/GetImageByDocId/SU-#{version.csv.second}"
   name "Power Device Network Utility"
   desc "Network device discovery utility"
   homepage "https://www.cyberpower.com/eu/en/software/utility"
