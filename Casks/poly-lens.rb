3e39e5cd78703ba26cc3f42e0d208715dfa38c40
John Benninghoff <jbenninghoff@mac.com>
2024-02-12 20:09:38 -0600

    Update poly-lens from 1.3.1 to 1.3.2

--- Casks/poly-lens.rb
+++ Casks/poly-lens.rb
@@ -1,6 +1,6 @@
 cask "poly-lens" do
-  version "1.3.1"
-  sha256 "a6c9933fc74d1aa3f1aa5a152937bbe0b65cb3386347f79478992f896c87ed17"
+  version "1.3.2"
+  sha256 "e3c3f4e01ae1d6f92d1dd44e0ba10fc1b34ba2910667112d8da007ee530c963b"
 
   url "https://swupdate.lens.poly.com/lens-desktop-mac/#{version}/#{version}/PolyLens-#{version}.dmg"
   name "poly-lens"
