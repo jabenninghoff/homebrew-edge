857928dc972a6453a4fc2ff49b0167176dfe38c3
John Benninghoff <jbenninghoff@mac.com>
2025-03-20 08:45:18 -0500

    Update poly-lens from 1.2.0 to 2.0.0

--- Casks/poly-lens.rb
+++ Casks/poly-lens.rb
@@ -1,8 +1,8 @@
 cask "poly-lens" do
-  version "1.4.0"
-  sha256 "5227acb464a58998f930d027da226fa337e27bd71a58ebec2ec415629f40045d"
+  version "2.0.0.632"
+  sha256 "3bdc4c407eb4390d4900f91dc9757f9d21a5af50551f3228818549f46b160a51"
 
-  url "https://swupdate.lens.poly.com/lens-desktop-mac/#{version}/#{version}/PolyLens-#{version}.dmg"
+  url "https://swupdate.lens.poly.com/lens-desktop-mac/#{version}/#{version}/LensDesktop-#{version}.dmg"
   name "poly-lens"
   desc "Configuration software for Poly devices"
   homepage "https://www.poly.com/us/en/support/downloads-apps"
