dff1b38309893642fd7fa8cdc391f57b02b803ab
John Benninghoff <jbenninghoff@mac.com>
2025-09-10 10:46:15 -0400

    Update poly-lens from 2.2.0 to 2.3.0

--- Casks/poly-lens.rb
+++ Casks/poly-lens.rb
@@ -1,11 +1,12 @@
 cask "poly-lens" do
-  version "2.2.0.1370"
-  sha256 "338c81c63e3c062f45221560f8274190a1156cdfafbbb7e46bbf0d22c8853f3a"
+  version "2.3.0.1517"
+  sha256 "9428e67219eb4485f0bff565775e70f5e4f94783f16b65ea5cf92e928f8dc080"
 
-  url "https://swupdate.lens.poly.com/lens-desktop-mac/#{version}/#{version}/LensDesktop-#{version}.dmg"
+  url "https://swupdate.lens.poly.com/lens-desktop-mac/#{version}/#{version}/LensDesktop-#{version}.dmg",
+      verified: "swupdate.lens.poly.com/lens-desktop-mac/"
   name "poly-lens"
   desc "Configuration software for Poly devices"
-  homepage "https://www.poly.com/us/en/support/downloads-apps"
+  homepage "https://www.hp.com/us-en/poly/software-and-services/software/poly-lens/app.html"
 
   livecheck do
     skip "Homepage version API not supported"
