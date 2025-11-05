6e0254a6c857b474e686b6ba0d925b2db1594d31
John Benninghoff <jbenninghoff@mac.com>
2025-11-05 15:55:35 -0600

    Rename powerpanel1 to powerpanel@1

--- Casks/powerpanel@1.rb
+++ Casks/powerpanel@1.rb
@@ -1,4 +1,4 @@
-cask "powerpanel1" do
+cask "powerpanel@1" do
   version "1.1.0"
   sha256 "9ef3160ad516cf246647a11bdbeaa756a98b2c0d9915ccabbf0d6cc72bebdf51"
 
@@ -8,7 +8,9 @@ cask "powerpanel1" do
   desc "Monitor and configure UPS"
   homepage "https://www.cyberpowersystems.com/products/software/power-panel-personal/"
 
-  depends_on macos: ">= :sierra"
+  livecheck do
+    skip "legacy version"
+  end
 
   app "PowerPanel.app"
 
