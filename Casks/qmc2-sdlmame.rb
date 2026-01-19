6c72d28f61e32ba9869147dec0ae2509ac669b3a
John Benninghoff <jbenninghoff@mac.com>
2026-01-19 14:27:23 -0600

    New cask: qmc2-sdlmame

--- Casks/qmc2-sdlmame.rb
+++ Casks/qmc2-sdlmame.rb
@@ -1,30 +1,22 @@
-# Documentation: https://docs.brew.sh/Cask-Cookbook
-#                https://docs.brew.sh/Adding-Software-to-Homebrew#cask-stanzas
-# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
 cask "qmc2-sdlmame" do
   arch arm: "AppleSilicon", intel: "MacIntel"
-  version "244,3"
-  sha256 arm:   "9c63c63c6027df1b4c5aeb9658434bcdf712e1800f65b97803ff0a3d9efed12d",
-         intel: "7df06b9989657cd3326b62aabff9e6591761c555ac30e5960ce209f2b628e598"
 
-  url "https://github.com/estefan3112/qmc2-mame-fe-MacOS/releases/download/v#{version.csv.first}v#{version.csv.second}/qmc2v#{version.csv.first}v#{version.csv.second}-#{arch}.zip"
+  version "244v4"
+  sha256 arm:   "7546fce99615448766a0b5b6476b984c790fe841fa80d10e300f97cd992667fb",
+         intel: "a6ab3917e36730c604b6319a2ca33ac3cca9d87845924d55aa3eb6aac065c9aa"
+
+  url "https://github.com/estefan3112/qmc2-mame-fe-MacOS/releases/download/v#{version}/qmc2v#{version}-#{arch}.zip"
   name "qmc2-sdlmame"
   desc "GUI front-end for MAME"
   homepage "https://github.com/estefan3112/qmc2-mame-fe-MacOS"
 
-  # Documentation: https://docs.brew.sh/Brew-Livecheck
-  livecheck do
-#    url ""
-#    strategy ""
-  end
-
   conflicts_with cask: "qmc2"
+  depends_on formula: "mame"
 
+  app "qmc2v#{version}-#{arch}/qmc2-sdlmame.app"
+  artifact "qmc2v#{version}-#{arch}/qmc2", target: "/Library/Application Support/qmc2"
 
-#  depends_on macos: ""
+  uninstall rmdir: "/Library/Application Support/qmc2"
 
-  app "qmc2-#{arch} #{version.csv.second}/qmc2-sdlmame.app"
-
-  # Documentation: https://docs.brew.sh/Cask-Cookbook#stanza-zap
-#  zap trash: ""
+  zap trash: "~/Library/Application Support/qmc2"
 end
