1ed1ccf0787fece74e21c5d94953438bf25ef41d
John Benninghoff <jbenninghoff@mac.com>
2026-09-07 13:26:15 -0500

    audit fix

--- Casks/macupdater.rb
+++ Casks/macupdater.rb
@@ -16,7 +16,7 @@ cask "macupdater" do
   end
 
   auto_updates true
-  depends_on macos: :monterey
+  depends_on macos: :ventura
 
   app "MacUpdater.app"
   binary "#{appdir}/MacUpdater.app/Contents/Resources/macupdater_install"
